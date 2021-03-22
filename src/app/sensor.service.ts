e951e0d38cc4ae4c837cd8649f8a09ba16d3bf39
RSTq1337 <37782356+RSTq1337@users.noreply.github.com>
2021-03-22 16:32:54 +0300

    Update sensor.service.ts

--- src/app/sensor.service.ts
+++ src/app/sensor.service.ts
@@ -13,23 +13,23 @@ export class SensorService {
   constructor(private http: HttpClient) { }
 
   getSensorList(): Observable<any> {
-    return this.http.get(`${this.baseUrl}` + 'sensor-list');
+    return this.http.get(`${this.baseUrl}`);
   }
 
   createSensor(sensor: object): Observable<object> {
-    return this.http.post(`${this.baseUrl}` + 'save-sensor', sensor);
+    return this.http.post(`${this.baseUrl}`, sensor);
   }
 
   deleteSensor(id: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/delete-sensor/${id}`, { responseType: 'text' });
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
   }
 
   getSensor(id: number): Observable<Object> {
-    return this.http.get(`${this.baseUrl}/sensor/${id}`);
+    return this.http.get(`${this.baseUrl}/${id}`);
   }
 
   updateSensor(id: number, value: any): Observable<Object> {
-    return this.http.post(`${this.baseUrl}/update-sensor/${id}`, value);
+    return this.http.post(`${this.baseUrl}/${id}`, value);
   }
 
 }
